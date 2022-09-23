4b80fae2e08fdb6c6539f7945e3d84e197b71989
Mateus Rosa <mateusrosa0901@gmail.com>
2022-09-23 17:37:42 -0300

    Update desafio 4.py

--- modulo 1/desafio/desafio 4.py
+++ modulo 1/desafio/desafio 4.py
@@ -1,3 +1,9 @@
-n1 = int(input('Digite um número: '))
+n1 = input('Digite um número: ')
 #mostrar qual o tipo de n1
-print(type(n1)) 
+print(f'tipo: {type(n1)}') 
+#diz se tem apenas espaço
+print(f'é espaço: {n1.isspace()}')
+#diz se é número
+print(f'é número: {n1.isnumeric()}')
+#diz se é palavra
+print(f'é palavra: {n1.isalpha()}')
