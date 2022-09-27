041cf5e25183583012868328cb8758493010ce56
Mateus Rosa <mateusrosa0901@gmail.com>
2022-09-27 18:40:03 -0300

    desafio 5 e 6

---
+++ modulo 1/desafio/desafio 5.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('='*20)
+print(f'antecessor: {a}')
+print(f'sucessor: {s}')
+print('='*20)
---
+++ modulo 1/desafio/desafio 6.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+d = n*2
+t = n*3
+r = n**(1/2)
+print(f'{r:.0f}')
