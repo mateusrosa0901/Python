039e754fd20114181d7353bd4e221b12a24f5a09
Mateus Rosa <mateusrosa0901@gmail.com>
2022-09-28 23:18:25 -0300

    aula 8.2 e 8.3

---
+++ modulo 1/aulas/aula 8.2.py
@@ -0,0 +1,3 @@
+import random
+ale = random.randint(1,20)
+print(ale)
---
+++ modulo 1/aulas/aula 8.3.py
@@ -0,0 +1,6 @@
+
+import imp
+import emoji
+
+import emoji
+print(emoji.emojize('Olá, mundo :terra_américas:' , language='pt'))
