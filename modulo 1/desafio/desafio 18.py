d1322bd9ef23d5499ff268652ab4ef161e338cd8
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-01 18:28:35 -0300

    Update desafio 18.py

--- modulo 1/desafio/desafio 18.py
+++ modulo 1/desafio/desafio 18.py
@@ -3,6 +3,6 @@ a = int(input('Digite um ângulo: '))
 
 r = math.radians(a)
 
-print(f'seno: {math.sin(r)}')
-print(f'cosseno: {math.cos(r)}')
-print(f'tangente: {math.tan(r)}')
+print(f'seno: {math.sin(r):.2f}')
+print(f'cosseno: {math.cos(r):.2f}')
+print(f'tangente: {math.tan(r):.2f}')
