0271af83e3d3adbf1d086f1a774bf8bebb669c77
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-01 18:41:01 -0300

    Update desafio 20.py

--- modulo 1/desafio/desafio 20.py
+++ modulo 1/desafio/desafio 20.py
@@ -1,5 +1,12 @@
+from binascii import a2b_base64
 import random
-a = ['André' , 'Mike' , 'Lívia' , 'Kyssila']
-random.shuffle(a)
+
+a1 = input('Digite o nome do 1° aluno: ')
+a2 = input('Digite o nome do 2° aluno: ')
+a3 = input('Digite o nome do 3° aluno: ')
+a4 = input('Digite o nome do 4° aluno: ')
+
+al = [a1 , a2 , a3 , a4]
+random.shuffle(al)
 print('Ordem de apresentação:')
-print(a)
+print(al)
