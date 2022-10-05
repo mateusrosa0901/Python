917af5049568767e379d62ded0d08e641a544be6
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-04 23:23:24 -0300

    Update desafio 26.py

--- modulo 1/desafio/desafio 26.py
+++ modulo 1/desafio/desafio 26.py
@@ -2,9 +2,9 @@ from itertools import count
 
 
 frase = input('Digite uma frase: ')
-mai = frase.upper()
-contA = mai.count('A')
+formatar = frase.upper().strip()
+contA = formatar.count('A')
 
 print(f'quantidadede A: {contA}')
-print(f'posição do 1° A: {mai.find("A")}')
-print(f'posição do ultimo A: {mai.rfind("A")}')
+print(f'posição do 1° A: {formatar.find("A")+1}')
+print(f'posição do ultimo A: {formatar.rfind("A")+1}')
