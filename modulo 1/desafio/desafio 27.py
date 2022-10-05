f0b0c3f2a056b32402c18c2bcabd906a2bea6c62
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-04 22:56:15 -0300

    Update desafio 27.py

--- modulo 1/desafio/desafio 27.py
+++ modulo 1/desafio/desafio 27.py
@@ -1,6 +1,5 @@
 nome = input('Digite seu nome: ')
 separar = nome.split()
-ContSeparar = len(separar)
 
 print(f'primeiro nome: {separar[0]}')
-print(f'ultimo nome: {separar[ContSeparar-1]}')
+print(f'ultimo nome: {separar[len(separar)-1]}')
