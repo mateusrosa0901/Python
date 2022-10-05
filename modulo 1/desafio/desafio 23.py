06c58ce8dd3f43d1e634cd120de91fbf0cdf4fd0
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-04 23:14:03 -0300

    Update desafio 23.py

--- modulo 1/desafio/desafio 23.py
+++ modulo 1/desafio/desafio 23.py
@@ -1,6 +1,10 @@
-n = input('Digite um número de 0 a 9999: ')
 
-print(f'unidade: {n[3]}')
-print(f'dezena: {n[2]}')
-print(f'centena: {n[1]}')
-print(f'milhar: {n[0]}')
+n = int(input('Digite um número de 0 a 9999: '))
+U = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
+print(f'unidade: {U}')
+print(f'dezena: {d}')
+print(f'centena: {c}')
+print(f'milhar: {m}')
