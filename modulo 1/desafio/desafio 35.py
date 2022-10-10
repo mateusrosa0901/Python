b9b2e316f8333a8759e8f9f6b8a90b3220a94a46
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-10 19:10:43 -0300

    Update desafio 35.py

--- modulo 1/desafio/desafio 35.py
+++ modulo 1/desafio/desafio 35.py
@@ -5,8 +5,5 @@ r3 = float(input('3° reta: '))
 if r1 + r2 > r3 and r2 + r3 > r1 and r1 + r3 > r2:
     print('Essas retas podem formar um triângulo.')
 
-elif r1 == r2 and r1 == r3 and r2 == r3:
-    print('Essas retas podem formar um triângulo.')
-
 else:
     print('Essas retas não podem formar um triângulo.')
