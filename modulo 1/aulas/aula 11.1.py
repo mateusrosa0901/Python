6d86fc943beea9e443e62508460162d47d476161
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-10 19:21:42 -0300

    Create aula 11.1.py

---
+++ modulo 1/aulas/aula 11.1.py
@@ -0,0 +1,10 @@
+print('=-='*30)
+#style
+print('\033[0;mNenhum')
+print('\033[1mNegrito')
+print('\033[4mSublinhado')
+print('\033[7mNegativo')
+print('=-='*30)
+#color txt
+print('\033[30mBranco')
+print('\033[31mVermelho')
