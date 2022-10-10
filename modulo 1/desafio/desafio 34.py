d5fbdc22b3b49de9eeb6aa4ba8e3dc27da1cb612
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-10 18:18:36 -0300

    Create desafio 34.py

---
+++ modulo 1/desafio/desafio 34.py
@@ -0,0 +1,9 @@
+sala = float(input('Digite o valor do salário: R$'))
+aum = 0
+if sala >= 1250:
+    aum = sala + (sala * 0,1)
+    print(f'O novo salário passa a ser R${aum:.2f}')
+
+else:
+    aum = sala + (sala+0,15)
+    print(f'O novo salário passa a ser R${aum:.2f}')
