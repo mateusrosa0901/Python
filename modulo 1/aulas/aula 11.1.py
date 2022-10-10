0497a94ab21cbcfac218bd4ece76b800f885663a
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-10 19:23:19 -0300

    Update aula 11.1.py

--- modulo 1/aulas/aula 11.1.py
+++ modulo 1/aulas/aula 11.1.py
@@ -1,10 +1,10 @@
 print('=-='*30)
 #style
-print('\033[0;mNenhum')
-print('\033[1mNegrito')
-print('\033[4mSublinhado')
-print('\033[7mNegativo')
+print('\033[0;mNenhum\033[m')
+print('\033[1mNegrito\033[m')
+print('\033[4mSublinhado\033[m')
+print('\033[7mNegativo\033[m')
 print('=-='*30)
 #color txt
-print('\033[30mBranco')
-print('\033[31mVermelho')
+print('\033[30mBranco\033[m')
+print('\033[31mVermelho\033[m')
