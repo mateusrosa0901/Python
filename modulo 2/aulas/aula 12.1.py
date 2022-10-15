f1c073cdedf1b39f7f54df31107ef37521577cc8
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-14 22:24:59 -0300

    Update aula 12.1.py

---
+++ modulo 2/aulas/aula 12.1.py
@@ -0,0 +1,12 @@
+nome = input('Digite seu nome: ')
+
+if nome == 'Mateus':
+    print(f'Que nome bonito!')
+
+elif nome == 'Enzo' or nome == 'Valentina':
+    print('Seu nome é popular no Brasil!')
+
+else:
+    print('Seu nome é bem normal!')
+
+print(f'Tenha um bom dia, {nome}!')
