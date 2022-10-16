aa20f84762c265fdcf28570c174a3f010bf101a8
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-16 01:13:21 -0300

    Update desafio 39.py

--- modulo 2/desafio/desafio 39.py
+++ modulo 2/desafio/desafio 39.py
@@ -1,1 +1,13 @@
+from datetime import date
 ano = int(input('Ano de nascimento: '))
+idade = date.today().year - ano
+
+if idade < 18:
+    print('Ainda não chegou sua hora de se alistar.')
+    print(f'Você deve se alistar em {(18-idade)+date.today().year}')
+
+elif idade == 18:
+    print('Já está no ano de você se alistar.')
+
+elif idade > 18:
+    print('Já passou o ano de você se alistar.')
