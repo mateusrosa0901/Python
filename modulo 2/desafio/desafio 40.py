e1012d2550056e779327eaec9b4d7079b2736203
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-16 01:26:56 -0300

    Create desafio 40.py

---
+++ modulo 2/desafio/desafio 40.py
@@ -0,0 +1,14 @@
+n1 = float(input('Digite a 1° nota: '))
+n2 = float(input('Digite a 2° nota: '))
+
+m = (n1 + n2) / 2
+print(f'{m:.1f}')
+
+if m <=5:
+    print('\033[31mREPROVADO\033')
+
+elif m <= 6.9:
+    print('\033[33mRECUPERAÇÃO\033')
+
+elif m >= 7:
+    print('\033[32mAPROVADO\033')
