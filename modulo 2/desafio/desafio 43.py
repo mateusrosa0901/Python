503755d07fd8c4d2dda5bcf08b4d10e6f37d0873
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-19 17:44:17 -0300

    Update desafio 43.py

--- modulo 2/desafio/desafio 43.py
+++ modulo 2/desafio/desafio 43.py
@@ -2,11 +2,15 @@ peso = float(input('Digite seu peso: '))
 altura = float(input('Digite sua altura: '))
 
 icm = peso / (altura*altura)
-print(icm:.2f)
+print(f'{icm:.1f}')
 
 if icm < 18.5:
     print('Abaixo do peso')
-elif icm >= 18.5 or icm < 25:
+elif icm >= 18.5 and icm < 25:
     print('Peso ideal')
-elif icm >= 25 or icm < 30:
+elif icm >= 25 and icm < 30:
     print('Sobrepeso')
+elif icm >= 30 and icm < 40:
+    print('Obesidade')
+elif icm >= 40:
+    print('Obesidade mórbida')
