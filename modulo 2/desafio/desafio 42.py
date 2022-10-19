0f5bef02f4b1dd38adc8552d4b2c67ae323ecbfd
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-19 17:27:21 -0300

    Update desafio 42.py

--- modulo 2/desafio/desafio 42.py
+++ modulo 2/desafio/desafio 42.py
@@ -6,7 +6,8 @@ r2 = float(input('2° reta: '))
 r3 = float(input('3° reta: '))
 
 if r1 + r2 > r3 and r2 + r3 > r1 and r1 + r3 > r2:
-    true = print('Essas retas podem formar um triângulo.')
+    true = 'Essas retas podem formar um triângulo.'
+    print(true)
 
 else:
     print('Essas retas não podem formar um triângulo.')
