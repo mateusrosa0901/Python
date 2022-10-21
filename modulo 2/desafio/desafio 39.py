61bc0c728ffa737b13ea6814bef291cf3cf61ac5
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-21 20:58:23 -0300

    Update desafio 39.py

--- modulo 2/desafio/desafio 39.py
+++ modulo 2/desafio/desafio 39.py
@@ -1,14 +1,26 @@
 from datetime import date
-ano = int(input('Ano de nascimento: '))
-idade = date.today().year - ano
+print('Qual o seu sexo:')
+print('1- Masculino')
+print('2- Feminino')
+opção = int(input('-> '))
+if opção == 1:
+    ano = int(input('Ano de nascimento: '))
+    idade = date.today().year - ano
 
-if idade < 18:
-    print('Ainda não chegou sua hora de se alistar.')
-    print(f'Você deve se alistar em {(18-idade)+date.today().year}')
+    if idade < 18:
+        print('Ainda não chegou sua hora de se alistar.')
+        print(f'Você deve se alistar em {(18-idade)+date.today().year}')
 
-elif idade == 18:
-    print('Já está no ano de você se alistar.')
+    elif idade == 18:
+        print('Já está no ano de você se alistar.')
 
-elif idade > 18:
-    print('Já passou o ano de você se alistar.')
-    print(f'Você deveria se alistar em {(18-idade)+date.today().year}')
+    elif idade > 18:
+        print('Já passou o ano de você se alistar.')
+        print(f'Você deveria se alistar em {(18-idade)+date.today().year}')
+
+elif opção == 2:
+    print('Mulheres não fazem alistamento militar no Brasil.')
+
+else:
+    print('Opção inválida')
+
