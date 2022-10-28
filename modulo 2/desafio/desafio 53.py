313cb084510abbead200970eef3b63f570ad5c07
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-28 12:52:16 -0300

    desafio 53 e 54

---
+++ modulo 2/desafio/desafio 53.py
@@ -0,0 +1,5 @@
+frase = input('Digite uma frase: ')
+ffrase = frase.upper().replace(' ','')
+
+for l in range(0,len(ffrase),-1):
+    print(l)
---
+++ modulo 2/desafio/desafio 54.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime
+ma = 0
+me = 0
+for p in range(1,8):
+    ano = int(input('Ano de nascimento: '))
+    i = date.today().year - ano
+    if i >= 18:
+        ma += 1
+    else:
+        me += 1
+
+print(f'{ma} pessoas são maiores de idade.')
+print(f'{me} pessoas são menores de idade.')
