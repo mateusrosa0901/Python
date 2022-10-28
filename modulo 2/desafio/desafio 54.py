cd02cd304653aae73f77b44ae24c044b6794a9c4
Mateus Rosa <mateusrosa0901@gmail.com>
2022-10-28 12:54:09 -0300

    Update desafio 54.py

--- modulo 2/desafio/desafio 54.py
+++ modulo 2/desafio/desafio 54.py
@@ -4,7 +4,7 @@ me = 0
 for p in range(1,8):
     ano = int(input('Ano de nascimento: '))
     i = date.today().year - ano
-    if i >= 18:
+    if i >= 21:
         ma += 1
     else:
         me += 1
