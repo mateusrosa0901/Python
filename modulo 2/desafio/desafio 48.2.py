dafc48cf62809a5ac386442f832664b01569b1b2
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-02 20:08:19 -0300

    Update desafio 48.2.py

--- modulo 2/desafio/desafio 48.2.py
+++ modulo 2/desafio/desafio 48.2.py
@@ -1,8 +1,8 @@
 ca = 0
 for c in range(1,501):
     print(c)
-    if c % 3 == 0:
-        if c % 2 == 1:
+    if c % 2 == 1:
+        if c % 3 == 0:
             ca += c
 
 print(f'Resultado: {ca}')
