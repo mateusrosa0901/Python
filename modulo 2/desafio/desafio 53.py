39fc64172fb9580a77f99fe1f2b9b356ef56d7cd
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-02 20:43:17 -0300

    Update desafio 53.py

--- modulo 2/desafio/desafio 53.py
+++ modulo 2/desafio/desafio 53.py
@@ -1,3 +1,14 @@
-frase = input('Digite uma frase: ').strip().upper().replace(' ' , '')
-for l in range(len(ffrase),0,-1):
-    print(l)
+frase = input('Digite uma frase: ').strip().upper()
+p = frase.split()
+j = ''.join(p)
+i = ''
+
+for l in range(len(j) -1, -1, -1):
+    i += j[l]
+
+if i == j:
+    print(j,i)
+    print('Essa frase é um palindromo.')
+else:
+    print(j,i)
+    print('Essa frase não é um palindromo.')
