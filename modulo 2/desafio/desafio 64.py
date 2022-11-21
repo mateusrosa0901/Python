c9cc68be62624cee4691cf272e985af2c2d3f250
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-21 16:39:24 -0300

    Create desafio 64.py

---
+++ modulo 2/desafio/desafio 64.py
@@ -0,0 +1,12 @@
+n = 0
+s = 0
+c = 0
+while n != 999:
+    print('='*50)
+    print('Para sair digite 999')
+    n = int(input('Digite um número: '))
+    if n != 999:
+        s += n
+        c += 1
+print(f'Você digitou {c} números.')
+print(f'{s} é a soma desses números.')
