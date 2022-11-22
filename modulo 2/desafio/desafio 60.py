77de988c560c21b9ebeb4ba98d86ff0efd55bbbe
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-21 21:33:03 -0300

    Update desafio 60.py

--- modulo 2/desafio/desafio 60.py
+++ modulo 2/desafio/desafio 60.py
@@ -1,10 +1,10 @@
-n = int(input('Digite um número para saber seu fatorial: '))
-c = 0
-f = 0
-fa = 0
-while c != 1:
-    c = n-1
-    f = n*c
-    fa = fa + f
+n = int(input('Digite um número para saber seu fatorial: ') )
 
-print(fa) 
+f = 1
+c = 1
+
+while c <= n:
+    f *= c
+    c += 1
+
+print(f)
