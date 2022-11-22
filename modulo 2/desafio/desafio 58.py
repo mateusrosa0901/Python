eab38a282505642071061dc347c6ed646a03ce24
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-22 18:08:36 -0300

    Update desafio 58.py

--- modulo 2/desafio/desafio 58.py
+++ modulo 2/desafio/desafio 58.py
@@ -3,12 +3,20 @@ c = 1
 
 na = random.randrange(0,10)
 nu = int(input('Digite um número de 0 a 10: '))
+if nu > na:
+    print('Menos... Tente novamente.')
+elif nu < na:
+    print('Mais... Tente novamente.')
 
 while nu != na:
     c += 1
     print('='*20)
     print('Você errou!')
     nu = int(input('Digite um número de 0 a 10: '))
+    if nu > na:
+        print('Menos... Tente novamente.')
+    elif nu < na:
+        print('Mais... Tente novamente.')
 
 
 print('Você acertou!')
