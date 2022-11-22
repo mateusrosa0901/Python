5a1028923eb53d76803f8bfb6946f2a727d36db3
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-21 21:43:21 -0300

    Create desafio 65.py

---
+++ modulo 2/desafio/desafio 65.py
@@ -0,0 +1,19 @@
+continuar = 'S'
+n = 0
+c = 0
+s = 0
+while continuar == 'S':
+    print('='*50)
+    n = int(input('Digite um número: '))
+    c += 1
+    s += n
+    continuar = input('Você deseja continuar? [S/N]: ').upper()
+    
+    if continuar != 'S' and continuar != 'N':
+        print('Valor inválido.')
+        continuar = input('Você deseja continuar? [S/N]: ').upper()
+
+print('='*50)
+print(f'Média dos números digitados: {s/c}')
+print(f'Soma dos números digitados: {s}')
+print('='*50)
