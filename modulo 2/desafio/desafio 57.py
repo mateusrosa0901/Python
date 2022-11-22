326515df1798bb20bbf970d70afddd42cb8cde12
Mateus Rosa <mateusrosa0901@gmail.com>
2022-11-22 18:02:42 -0300

    Update desafio 57.py

--- modulo 2/desafio/desafio 57.py
+++ modulo 2/desafio/desafio 57.py
@@ -1,9 +1,9 @@
-s = input('Digite seu sexo[F/M]: ').upper()
+s = input('Digite seu sexo[F/M]: ').upper().strip()
 
 while s != 'F' and s != 'M':
     print('='*20)
     print('Valor inválido, tente novamente.')
-    s = input('Digite seu sexo[F/M]: ').upper()
+    s = input('Digite seu sexo[F/M]: ').upper().strip()
     
 if s == 'M':
     print('Sexo masculino registrado.')
