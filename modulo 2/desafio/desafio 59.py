4d83e345530948f19819d816570da935c4700680
Mateus Rosa <mateusrosa0901@gmail.com>
2022-12-12 21:02:47 -0300

    Update desafio 59.py

--- modulo 2/desafio/desafio 59.py
+++ modulo 2/desafio/desafio 59.py
@@ -1,6 +1,7 @@
 o = 0
 n1 = int(input('Digite o 1° número: '))
 n2 = int(input('Digite o 2° número: '))
+
 while o != 5:
     print('='*50)
     
@@ -21,13 +22,16 @@ while o != 5:
             o = int(input('Opção inválida, digite novamente: '))
     elif o == 1:
         print(f'{n1} + {n2} = {n1+n2}')
+
     elif o == 2:
         print(f'{n1} X {n2} = {n1*n2}')
+
     elif o == 3:
         if n1 > n2:
             print(f'{n1} é maior que {n2}')
         else:
             print(f'{n2} é maior que {n1}')
+
     elif o == 4:
         n1 = int(input('Digite o 1° número: '))
         n2 = int(input('Digite o 2° número: '))
