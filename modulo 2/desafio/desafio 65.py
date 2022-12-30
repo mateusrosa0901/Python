8cad2143f83e6448f537bdb493b2b43bf5883d6c
Mateus Rosa <mateusrosa0901@gmail.com>
2022-12-30 18:50:40 -0300

    Update desafio 65.py

--- modulo 2/desafio/desafio 65.py
+++ modulo 2/desafio/desafio 65.py
@@ -2,12 +2,24 @@ continuar = 'S'
 num = 0
 cont = 0
 soma = 0
+maior = 0
+menor = 0
 
 while continuar == 'S':
     print('='*50)
     num = int(input('Digite um número: '))
     cont += 1
     soma += num
+    if cont == 1:
+        maior = num
+        menor = num
+    
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+
     continuar = input('Você deseja continuar? [S/N]: ').upper().strip()[0]
     
     while continuar != 'S' and continuar != 'N':
@@ -17,4 +29,6 @@ while continuar == 'S':
 print('='*50)
 print(f'Média dos números digitados: {soma/cont}')
 print(f'Soma dos números digitados: {soma}')
+print(f'Maior número digitado: {maior}')
+print(f'Menor número digitado: {menor}')
 print('='*50)
