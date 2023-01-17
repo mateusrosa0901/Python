a738ea0abf6565227bac6de2bfc1aacd844d4498
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-17 15:45:54 -0300

    Update desafio 71.py

--- modulo 2/desafio/desafio 71.py
+++ modulo 2/desafio/desafio 71.py
@@ -1,8 +1,24 @@
 valor = int(input('Digite o valor que você deseja sacar: R$'))
 total = valor
-ced = 50
-totced = 0
+ced = int(50)
+totced = int(0)
 
+print('---------- RESUMO ----------')
 while True:
     if total >= ced:
-        total
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'{totced} notas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+
+print('-'*28)
