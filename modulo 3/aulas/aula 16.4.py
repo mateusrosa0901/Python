946ab0d65e4c5a3350d83816c9a63bef5dd82d92
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-17 21:13:54 -0300

    Update aula 16.4.py

--- modulo 3/aulas/aula 16.4.py
+++ modulo 3/aulas/aula 16.4.py
@@ -5,3 +5,4 @@ n3 = n1 + n2
 print(n3)
 print(n3.count(5)) #conta quantas vezes aparece o número 5
 print(n3.index(3)) #mostra a posição do número 3
+print(n3.index(5,3)) #o número após a vírgula define qual posição vai começar a 'varredura'
