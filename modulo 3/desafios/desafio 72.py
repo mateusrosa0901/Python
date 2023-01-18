da5e94fdc7c6c3d0ec58cfe9022d95d8548363bb
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-17 21:21:33 -0300

    Update desafio 72.py

--- modulo 3/desafios/desafio 72.py
+++ modulo 3/desafios/desafio 72.py
@@ -1,6 +1,7 @@
 numExtenso = ('zero' , 'um' , 'dois' , 'três' , 'quatro' , 'cinco' , 'seis' , 'sete' , 'oito' , 'nove' , 'dez')
 
 while True:
+    print('-'*20)
     num = int(input('Digite um número de 1 até 10: '))
     if num > 10:
         break
