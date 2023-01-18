6cbcf444c5fe05f2ef94663db376d27a380b7507
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-17 21:39:28 -0300

    Create desafio 73.py

---
+++ modulo 3/desafios/desafio 73.py
@@ -0,0 +1,9 @@
+tabelaBrasileirao = ('Palmeiras' , 'Internacional' , 'Fluminense' , 'Corinthians' , 'Flamengo' , 'Athletico' , 'Atlético MG' , 'Fortaleza' , 'São Paulo' , 'América' , 'Botafogo' , 'Santos' , 'Goiás' , 'Bragantino' , 'Coritiba' , 'Cuiabá' , 'Ceará' , 'Atlético GO' , 'Avaí' , 'Juventude')
+
+print(f'Os 5 primeiros colocados: {tabelaBrasileirao[0:5]}')
+print('-'*100)
+print(f'Os 4 últimos colocados: {tabelaBrasileirao[-4:]}')
+print('-'*100)
+print(f'Times em ordem alfabética: {sorted(tabelaBrasileirao)}')
+print('-'*100)
+print(f'Posição do atlético MG: {tabelaBrasileirao.index()}')
