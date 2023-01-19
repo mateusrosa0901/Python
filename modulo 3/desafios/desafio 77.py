a2ae059c065bf171f8cc6be88fd9cbf83b9cae28
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-19 16:01:58 -0300

    Update desafio 77.py

---
+++ modulo 3/desafios/desafio 77.py
@@ -0,0 +1,7 @@
+palavras = ('carro' , 'vaca' , 'lixo' , 'grama')
+
+for p in palavras:
+    print(f'\nA palavra {p.upper()} possui as vogais: ' , end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra)
