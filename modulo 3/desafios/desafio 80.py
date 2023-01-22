34a3b3bf6544609bb9fa7f64fab6700f461e3392
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-22 00:21:47 -0300

    Update desafio 80.py

--- modulo 3/desafios/desafio 80.py
+++ modulo 3/desafios/desafio 80.py
@@ -2,13 +2,13 @@ valores = []
 
 for c in range(0,5):
     num = int(input(f'Digite o {c+1}° valor: '))
-
     if c == 0:
         valores.append(num)
-
     else:
-        for n in range(0 , len(valores)):
-            if num > valores[n]:
-                valores.insert(n+1 , num)
+        for n in range(len(valores)):
+            if num > valores[n] and num < valores[n+1]:
+                valores.insert(valores.index(n+1) , num)
+            #if num < n:
+                #valores.insert(valores.index(n) , num)
 
 print(valores)
