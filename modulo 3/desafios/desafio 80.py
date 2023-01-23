43002e4d0c42bbbc1bd3249abb3c3bb7215062fe
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-22 22:17:09 -0300

    Update desafio 80.py

--- modulo 3/desafios/desafio 80.py
+++ modulo 3/desafios/desafio 80.py
@@ -4,6 +4,7 @@ for c in range(0,5):
     num = int(input(f'Digite o {c+1}° valor: '))
     if c == 0:
         valores.append(num)
+
     else:
         for n in range(len(valores)):
             if num > valores[n] and num < valores[n+1]:
