2a993149cd3dad30a760b4ce38c98ac8b6f05e1e
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-30 19:15:00 -0300

    Update aula 18.5.py

--- modulo 3/aulas/aula 18.5.py
+++ modulo 3/aulas/aula 18.5.py
@@ -8,3 +8,9 @@ for c in range(0, 3):
     dados.clear()
 
 print(galera)
+
+for p in galera:
+    if p[1] >= 18:
+        print(f'{p[0]} é maior de idade.')
+    else:
+        print(f'{p[0]} é menor de idade.')
