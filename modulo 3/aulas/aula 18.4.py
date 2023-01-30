3bb26bae7323f8926603eb824f08b6c671483c9b
Mateus Rosa <mateusrosa0901@gmail.com>
2023-01-30 19:07:28 -0300

    Create aula 18.4.py

---
+++ modulo 3/aulas/aula 18.4.py
@@ -0,0 +1,8 @@
+galera = [['Mateus', 18], ['Júlia', 13], ['Maria', 49]]
+
+for c in range(len(galera)):
+    print(f'{galera[c][0]} tem {galera[c][1]} anos de idade.')
+
+#for p in galera:
+#    print(f'{p[0]} tem {p[1]} anos de idade')
+    
