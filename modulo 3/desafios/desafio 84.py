a96cba2d781f0a12c8f80c9c22ecf6d28e622c0d
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-02 17:31:22 -0300

    Update desafio 84.py

--- modulo 3/desafios/desafio 84.py
+++ modulo 3/desafios/desafio 84.py
@@ -1,14 +1,27 @@
 dados = []
 lista = []
 loop = ''
-
+maiorPeso = 0
+menorPeso = 0
+cont = 0
 
 while True:
     print('-'*20)
-    dados.append(str(input('Digite o nome: ')))
-    dados.append(float(input('Digite o peso [Kg]: ')))
+    nome = str(input('Digite o nome: '))
+    peso = float(input('Digite o peso [Kg]: '))
+    dados.append(nome)
+    dados.append(peso)
     lista.append(dados[:])
     dados.clear()
+
+    if cont == 0:
+        maiorPeso = peso
+        menorPeso = peso
+    else:
+        if peso > maiorPeso:
+            maiorPeso = peso
+        if peso < menorPeso:
+            menorPeso = peso
 
     loop = str(input('Deseja cadastrar mais uma pessoa? [S/N]: ')).strip().upper()[0]
     while loop != 'S' and loop != 'N':
@@ -17,5 +30,18 @@ while True:
     if loop == 'N':
         break
 
+    cont += 1
 
+print('')
+print('-=-'*10)
 print(f'Quantidade de pessoas cadastradas: {len(lista)}')
+
+print(f'Nome das pessoas mais pesadas: {maiorPeso:.2f}Kg', end=' ')
+for p in lista:
+    if p[1] == maiorPeso:
+        print(p[0], end='; ')
+
+print(f'\nNome das pessoas mais leves: {menorPeso:.2f}Kg', end=' ')
+for p in lista:
+    if p[1] == menorPeso:
+        print(p[0], end='; ')
