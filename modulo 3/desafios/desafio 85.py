539f14573d0ec97a7fe0c8a0d21588a7c8bac60a
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-02 17:59:13 -0300

    Update desafio 85.py

--- modulo 3/desafios/desafio 85.py
+++ modulo 3/desafios/desafio 85.py
@@ -1,18 +1,32 @@
-numP = []
-numI = []
-num = []
+#numP = []
+#numI = []
+#num = []
+#numAux = 0
+
+#for c in range(0, 7):
+#    numAux = int(input('Digite um número: '))
+#    if numAux % 2 == 0:
+#        numP.append(numAux)
+#    else:
+#        numI.append(numAux)
+
+#numP.sort()
+#numI.sort()
+#num.append(numP[:])
+#num.append(numI[:])
+
+#print(num)
+
+num = [[], []]
 numAux = 0
 
 for c in range(0, 7):
     numAux = int(input('Digite um número: '))
     if numAux % 2 == 0:
-        numP.append(numAux)
+        num[0].append(numAux)
     else:
-        numI.append(numAux)
+        num[1].append(numAux)
 
-numP.sort()
-numI.sort()
-num.append(numP[:])
-num.append(numI[:])
-
+num[0].sort()
+num[1].sort()
 print(num)
