594efea7f7c9f385f9bc16dcd8a6516e1ef7be61
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-06 16:08:28 -0300

    Create desafio 88.py

---
+++ modulo 3/desafios/desafio 88.py
@@ -0,0 +1,26 @@
+import random
+from time import sleep
+jogos = []
+jogosAux = []
+qtdJogos = int(0)
+num = int(0)
+cont = 1
+
+qtdJogos = int(input('Quantos jogos você quer gerar? '))
+
+for con in range(0, qtdJogos):
+    for c in range(0, 6):
+        num = random.randrange(61)
+        while num in jogosAux:
+            num = random.randrange(61)
+        jogosAux.append(num)
+    jogos.append(jogosAux[:])
+    jogosAux.clear()
+
+print('='*20)
+print(f'- JOGOS -')
+for jogo in jogos:
+    print(f'jogo {cont}: {jogo[:]}')
+    sleep(1)
+    cont += 1
+print('='*20)
