f41a9119de3d2246b5330949b791357c775db1dc
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-09 15:45:35 -0300

    Update desafio 92.py

--- modulo 3/desafios/desafio 92.py
+++ modulo 3/desafios/desafio 92.py
@@ -7,17 +7,17 @@ anoNasc = int(input('Digite o ano de nascimento: '))
 idade = date.today().year - anoNasc
 ficha['idade'] = idade
 
-ficha['ctps'] = str(input('Carteira de trabalho: '))
+ficha['ctps'] = int(input('Carteira de trabalho: '))
 
 if ficha['ctps'] != 0:
-    ficha['anoContrato'] = int(input('Ano da sua contratação: '))
+    ficha['contrataçao'] = int(input('Ano da sua contratação: '))
     ficha['salario'] = float(input('Digite seu salário: R$'))
-    diferença = date.today().year - ficha['anoContrato']
-    idadeAux = idade - diferença
+    diferença = date.today().year - ficha['contrataçao']
+    idadeAposen = idade - diferença
     for c in range(0, 35):
-        idadeAux += 1
-    ficha['idadeAposentado'] = idadeAux
+        idadeAposen += 1
+    ficha['idadeAposentado'] = idadeAposen
 
+print('-=-'*20)
 for k, v in ficha.items():
     print(f'{k}: {v}')
-
