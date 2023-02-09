52641ad3ca6b5d91e7de3f839409007e3af97ee5
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-09 16:01:45 -0300

    Create desafio 93.py

---
+++ modulo 3/desafios/desafio 93.py
@@ -0,0 +1,25 @@
+fichaJogador = dict()
+gols = list()
+totGols = int(0)
+
+fichaJogador['nome'] = str(input('Digite o nome do jogador: '))
+
+partidas = int(input(f'Quantas partidas o {fichaJogador["nome"]} jogou? '))
+
+for p in range(partidas):
+    gols.append(int(input(f'Quantos gols foram feitos na partida {p}? ')))
+
+fichaJogador['gols'] = gols[:]
+
+for g in gols:
+    totGols += g
+
+fichaJogador['total'] = totGols
+
+print('-=-'*20)
+for k, v in fichaJogador.items():
+    print(f'{k}: {v}')
+
+print('-=-'*20)
+for g in range(len(gols)):
+    print(f'Na partida {g}, fez {gols[g]} gols.')
