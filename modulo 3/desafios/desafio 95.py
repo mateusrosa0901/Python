5f386fe5ba0308622dc79e49226dda9800e094e3
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-09 19:00:03 -0300

    Create desafio 95.py

---
+++ modulo 3/desafios/desafio 95.py
@@ -0,0 +1,52 @@
+ficha = {}
+jogadores = []
+gols = []
+totGols = int(0)
+cod = int(0)
+
+while True:
+    ficha['nome'] = str(input('Digite o nome do jogador: '))
+
+    partidas = int(input(f'Quantas partidas o {ficha["nome"]} jogou? '))
+
+    for p in range(partidas):
+        gols.append(int(input(f'Quantos gols foram feitos na partida {p}? ')))
+
+    ficha['gols'] = gols[:]
+
+    for g in gols:
+        totGols += g
+
+    ficha['total'] = totGols
+    totGols = int(0)
+
+    jogadores.append(ficha.copy())
+    ficha.clear()
+
+    loop = str(input('Adicionar mais uma pessoa: [S/N]: ')).strip().upper()[0]
+
+    while loop != 'S' and loop != 'N':
+        loop = str(input('Adicionar mais uma pessoa: [S/N]: ')).strip().upper()[0]
+    print('-'*25)
+
+    if loop == 'N':
+        break
+print('-=-'*20)
+print('id | nome | gols por partida | total')
+for j in range(len(jogadores)):
+    print(f'{j} | {jogadores[j]["nome"]} | {jogadores[j]["gols"]} | {jogadores[j]["total"]}')
+
+while True:
+    cod = int(input('Digite o id para ver mais detalhes: '))
+    if cod == 999:
+        break
+
+    while cod > len(jogadores) - 1:
+        cod = int(input('Digite o id para ver mais detalhes: '))
+
+    print(f'O jogador {jogadores[cod]["nome"]}, jogou {len(jogadores[cod]["gols"])} partidas:')
+    for g in range(len(jogadores[cod]["gols"])):
+        print(f'   -> Na partida {g}, fez {jogadores[cod]["gols"][g]} gols.')
+    print(f'Um total de {jogadores[cod]["total"]} gols.')
+
+    print('='*30)
