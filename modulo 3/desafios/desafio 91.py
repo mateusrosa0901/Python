664367182e3786c7949a923cb126f6ba003a4aad
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-10 17:19:04 -0300

    Update desafio 91.py

--- modulo 3/desafios/desafio 91.py
+++ modulo 3/desafios/desafio 91.py
@@ -8,6 +8,6 @@ jogadores['jogador2'] = randint(1, 6)
 jogadores['jogador3'] = randint(1, 6)
 jogadores['jogador4'] = randint(1, 6)
 
-for j in sorted(jogadores, key= jogadores.get, reverse= True):
+for j in sorted(jogadores, key=jogadores.get, reverse=True):
     print(f'O {j} ficou em {cont}° lugar: {jogadores[j]}')
     cont += 1
