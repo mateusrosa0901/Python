aae165653c801d0d6e8fdd89fca729de944208c5
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-10 17:09:44 -0300

    Update desafio 90.py

--- modulo 3/desafios/desafio 90.py
+++ modulo 3/desafios/desafio 90.py
@@ -6,8 +6,10 @@ ficha['media'] = float(input('Digite a média do aluno: '))
 if ficha['media'] >= 7:
     ficha['situacao'] = 'APROVADO'
 
+elif 7 > ficha['media'] >= 5:
+    ficha['situacao'] = 'RECUPERAÇÃO'
 else:
-    ficha['media'] = 'REPROVADO'
+    ficha['situacao'] = 'REPROVADO'
 
 print(f'nome | média | situação')
 print(f'{ficha["nome"]} | {ficha["media"]:.1f} | {ficha["situacao"]}')
