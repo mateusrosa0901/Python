ef8bb31141f6657926d25b6ebd618ab421d1a201
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-16 20:18:50 -0300

    Update desafio 99.py

--- modulo 3/desafios/desafio 99.py
+++ modulo 3/desafios/desafio 99.py
@@ -1,12 +1,15 @@
 def maior(* num):
     print('='*15)
     m = 0
+    c = 0
     for n in num:
         print(n, end=' -> ')
         if n >= m:
             m = n
+        c += 1
+
     print('FIM')
-    print(f'O maior número é: {m}')
+    print(f'{c} números foram informados e o maior é: {m}')
     print('='*15)
 
 
