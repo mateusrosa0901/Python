2a78fac4f20a874a4d88f0fd444f39081824c1d3
Mateus Rosa <mateusrosa0901@gmail.com>
2023-02-16 20:23:08 -0300

    Update desafio 99.py

--- modulo 3/desafios/desafio 99.py
+++ modulo 3/desafios/desafio 99.py
@@ -1,6 +1,6 @@
-
+from time import sleep
 def maior(* num):
-    print('='*15)
+    print('='*len(num))
     m = 0
     c = 0
     for n in num:
@@ -11,8 +11,9 @@ def maior(* num):
 
     print('FIM')
     print(f'{c} números foram informados e o maior é: {m}')
-    print('='*15)
+    print('='*len(num))
 
 
 maior(5, 8, 4, 7, 45, 8, 7, 6, 3, 5, 1)
+sleep(3)
 maior(2, 3, 5, 12, 47, 85, 41, 78, 93, 62)
