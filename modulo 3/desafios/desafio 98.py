f300b52ff90db7b6e71b0fe3f235fd64f21c1810
Mateus Rosa <mateusrosa0901@gmail.com>
2024-03-26 00:06:59 -0300

    removendo funções obsoletas
    essas funções não precisam ser mais usadas pois refiz a função contador, que supri todas as necessidades

--- modulo 3/desafios/desafio 98.py
+++ modulo 3/desafios/desafio 98.py
@@ -1,21 +1,4 @@
-def contador1(i, f, p):
-    for n in range(i, f+1, p):
-        print(n, end=' -> ')
-    print('FIM')
-
-    print('-=-'*10)
-
-
-def contador2(i, f, p):
-
-    for n in range(i, f-1, p):
-        print(n, end=' -> ')
-    print('FIM')
-
-    print('-=-'*10)
-
-
-def contador3(i, f, p):
+def contador(i, f, p):
 
     if i > f:
         f -= 1
@@ -41,11 +24,11 @@ def mudaPasso(i, f, p):
 
     return p
 
-contador1(1, 10, 1)
-contador2(10, 0, -2)
+contador(1, 10, 1)
+contador(10, 0, -2)
 
 ini = int(input('Digite o número de início: '))
 fim = int(input('Digite o número de fim: '))
 passo = int(input('Digite quantos números a contagem vai pular: '))
 
-contador3(ini, fim, passo)
+contador(ini, fim, passo)
