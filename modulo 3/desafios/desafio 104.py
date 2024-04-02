2210948f9f53c63f6963af718c760c9f15f251bb
Mateus Rosa <mateusrosa0901@gmail.com>
2024-04-02 20:07:51 -0300

    adicionando doc string

--- modulo 3/desafios/desafio 104.py
+++ modulo 3/desafios/desafio 104.py
@@ -1,4 +1,8 @@
 def leiaInt():
+    '''
+    Lê um número inteiro e caso ocorra um ValueError repete a leitura.
+    :return: O número inteiro digitado
+    '''
     print('-'*20)
     while True:
         try:
@@ -9,5 +13,5 @@ def leiaInt():
         except ValueError:
             print(f'\033[1;31mTipo inválido digite novamente\033[m')
 
-
+help(leiaInt)
 print(f'Numero digitado: {leiaInt()}')
