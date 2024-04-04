1b90adec94a5ce3abd292a04155239ef39ed365f
Mateus Rosa <mateusrosa0901@gmail.com>
2024-04-03 22:42:36 -0300

    Resolvendo o desafio 106

---
+++ modulo 3/desafios/desafio 106.py
@@ -0,0 +1,35 @@
+from time import sleep
+def inicio():
+    while True:
+        sleep(1)
+        print('\033[1;97;45m='*200)
+        print('PROGRAMA PyHelp')
+        print('=' * 200)
+
+        sleep(1)
+        func = str(input('\033[mFunção >> '))
+        if func == 'sair':
+            sair()
+            break
+
+        pyHelp(func)
+
+
+def pyHelp(func):
+    sleep(0.5)
+    print('\033[1;97;46m-' * 200)
+    print(f'Acessando manual do {func}')
+    print('-' * 200, '\033[1;40;47m')
+
+    sleep(0.5)
+    return help(func)
+
+
+def sair():
+    sleep(0.5)
+    print('\033[1;97;41m=' * 200)
+    print('Até mais tarde :)')
+    print('=' * 200)
+
+
+inicio()
