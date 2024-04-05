92e520cbe73f9e5e6751a9dbc3c978f224b0fdbc
Mateus Rosa <mateusrosa0901@gmail.com>
2024-04-05 11:47:18 -0300

    desafio 113

---
+++ modulo 3/desafios/desafio 113.py
@@ -0,0 +1,35 @@
+def leiaInt():
+    while True:
+        try:
+            inteiro = int(input('Digite um número inteiro: '))
+
+            return inteiro
+
+        except ValueError:
+            print('\033[1;91mTipo digitado inválido\033[m')
+
+        except KeyboardInterrupt:
+            print('\033[1;91mO usuário decidiu sair.\033[m')
+            return 0
+
+
+def leiaFloat():
+    while True:
+        try:
+            real = float(input('Digite um número inteiro: '))
+
+            return real
+
+        except ValueError:
+            print('\033[1;91mTipo digitado inválido\033[m')
+
+        except KeyboardInterrupt:
+            print('\033[1;91mO usuário decidiu sair.\033[m')
+            return 0
+
+
+i = leiaInt()
+r = leiaFloat()
+
+print(f'O número inteiro digitado foi: {i}')
+print(f'O número real digitado foi: {r}')
